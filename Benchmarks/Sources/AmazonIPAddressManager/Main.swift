de0fc5d69d8cd2bf35e7c4c86414e2b860971e53
kelvin13 <kelvin13ma@gmail.com>
2022-07-29 16:57:48 -0500

    add AWS IPAM benchmark, move benchmarks into separate SPM project

---
+++ Benchmarks/Sources/AmazonIPAddressManager/Main.swift
@@ -0,0 +1,111 @@
+import ArgumentParser
+import Foundation
+import JSON
+import SystemExtras
+
+struct Log:Decodable 
+{
+    let timestamp:String
+    let level:String
+    let thread:String
+    let logger:String
+    let message:String
+    
+    enum CodingKeys:String, CodingKey 
+    {
+        case timestamp = "@timestamp"
+        case level
+        case thread = "thread_name"
+        case logger = "logger_name"
+        case message
+    }
+
+    init(from json:JSON) throws 
+    {
+        (
+            timestamp:  self.timestamp,
+            level:      self.level,
+            thread:     self.thread,
+            logger:     self.logger,
+            message:    self.message
+        ) = try json.lint 
+        {
+            (
+                timestamp:  try $0.remove(CodingKeys.timestamp.rawValue, as: String.self),
+                level:      try $0.remove(CodingKeys.level.rawValue, as: String.self),
+                thread:     try $0.remove(CodingKeys.thread.rawValue, as: String.self),
+                logger:     try $0.remove(CodingKeys.logger.rawValue, as: String.self),
+                message:    try $0.remove(CodingKeys.message.rawValue, as: String.self)
+            )
+        }
+    }
+}
+
+@main 
+struct Main:ParsableCommand
+{
+    @Argument(help: "path to test data file (aws ip address manager log, jsonl format)")
+    var path:String 
+
+    func run() throws
+    {
+        let data:String = try FilePath.init(self.path).read()
+        let clock:SuspendingClock = .init()
+
+        let swiftJSONWithLinter:Duration = try clock.measure 
+        {
+            let logs:[Log] = try Self.benchmarkSwiftJSONWithLinter(data)
+            print("swift-json: decoded \(logs.count) logs")
+        }
+        let swiftJSON:Duration = try clock.measure 
+        {
+            let logs:[Log] = try Self.benchmarkSwiftJSON(data)
+            print("swift-json: decoded \(logs.count) logs")
+        }
+        let foundation:Duration = try clock.measure 
+        {
+            let logs:[Log] = try Self.benchmarkFoundation(data)
+            print("foundation: decoded \(logs.count) logs")
+        }
+        print("swift-json decoding time (fast decoding api): \(swiftJSONWithLinter)")
+        print("swift-json decoding time (compatibility api): \(swiftJSON)")
+        print("foundation decoding time:                     \(foundation)")
+    }
+    
+    @inline(never)
+    static 
+    func benchmarkSwiftJSONWithLinter(_ string:String) throws -> [Log]
+    {
+        var logs:[Log] = []
+        var input:ParsingInput<Grammar.NoDiagnostics<String.UTF8View>> = .init(string.utf8)
+        while let log:[(key:String, value:JSON)] = 
+            input.parse(as: JSON.Rule<String.Index>.Object?.self)
+        {
+            logs.append(try .init(from: .object(log)))
+        }
+        return logs
+    }
+    @inline(never)
+    static 
+    func benchmarkSwiftJSON(_ string:String) throws -> [Log]
+    {
+        var logs:[Log] = []
+        var input:ParsingInput<Grammar.NoDiagnostics<String.UTF8View>> = .init(string.utf8)
+        while let log:[(key:String, value:JSON)] = 
+            input.parse(as: JSON.Rule<String.Index>.Object?.self)
+        {
+            logs.append(try .init(from: JSON.object(log) as any Decoder))
+        }
+        return logs
+    }
+    @inline(never)
+    static 
+    func benchmarkFoundation(_ string:String) throws -> [Log]
+    {
+        let decoder:JSONDecoder = .init()
+        return try string.split(whereSeparator: \.isNewline).map 
+        { 
+            try decoder.decode(Log.self, from: $0.data(using: .utf8) ?? .init()) 
+        }
+    }
+}
---
+++ Benchmarks/Sources/GeneralDecoding/Main.swift
@@ -0,0 +1,63 @@
+import ArgumentParser
+import Foundation
+import JSON
+import SystemExtras
+
+@main 
+struct Main:ParsableCommand
+{
+    @Argument(help: "path to test data file")
+    var path:String 
+
+    func run() throws
+    {
+        let data:[UInt8] = try FilePath.init(self.path).read()
+
+        // the irony here is that ``Foundation/JSONDecoder`` cannot break message boundaries, 
+        // so we need to use `/swift-json`’s parser. 
+        var input:ParsingInput<Grammar.NoDiagnostics<[UInt8]>> = .init(data)
+        var boundaries:[Range<Int>] = []
+        var start:Int = input.index 
+        while let _:JSON = input.parse(as: JSON.Rule<Int>.Root?.self)
+        {
+            boundaries.append(start ..< input.index)
+            start = input.index
+        }
+
+        let clock:SuspendingClock = .init()
+
+        let us:Duration = try clock.measure 
+        {
+            let parsed:Int = try Self.benchmarkSwiftJSON(data)
+            print("swift-json: decoded \(parsed) messages")
+        }
+        let them:Duration = try clock.measure 
+        {
+            let parsed:Int = try Self.benchmarkFoundation(data, boundaries: boundaries)
+            print("foundation: decoded \(parsed) messages")
+        }
+        print("swift-json decoding time: \(us)")
+        print("foundation decoding time: \(them)")
+    }
+    
+    @inline(never)
+    static 
+    func benchmarkSwiftJSON(_ json:[UInt8]) throws -> Int
+    {
+        try Grammar.parse(json, as: JSON.Rule<Int>.Root.self, in: [JSON].self).count
+    }
+    @inline(never)
+    static 
+    func benchmarkFoundation(_ json:[UInt8], boundaries:[Range<Int>]) throws -> Int
+    {
+        let decoder:JSONDecoder = .init()
+        var count:Int           = 0
+        for range:Range<Int> in boundaries 
+        {
+            let data:Data   = .init(json[range])
+            let _:JSON      = try decoder.decode(JSON.self, from: data)
+            count          += 1
+        }
+        return count
+    }
+}
