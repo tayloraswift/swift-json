0246986d7f5d1785dc6e6f4fff408320b692001b
Tobias <t089@users.noreply.github.com>
2023-12-05 22:25:02 +0100

    Adds platform requirements in line with swift-grammar (#74)

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ var plugins:[Package.Dependency] = []
 
 let package:Package = .init(
     name: "swift-json",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products:
     [
         .library(name: "JSON", targets: ["JSON"]),
