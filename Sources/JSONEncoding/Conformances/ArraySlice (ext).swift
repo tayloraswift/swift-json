633b4bda79c8e28e1edc17974f95655c5a203f42
taylorswift <mdianna1989@gmail.com>
2024-05-24 02:05:28 +0000

    conform Array and ArraySlice to JSONEncodable

---
+++ Sources/JSONEncoding/Conformances/ArraySlice (ext).swift
@@ -0,0 +1,13 @@
+extension ArraySlice:JSONEncodable where Element:JSONEncodable
+{
+    @inlinable public
+    func encode(to json:inout JSON)
+    {
+        {
+            for element:Element in self
+            {
+                $0[+] = element
+            }
+        } (&json[as: JSON.ArrayEncoder.self])
+    }
+}
